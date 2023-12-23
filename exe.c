29c2e132b4f510e0dd514a88ccb51dbb9710f210
sarabouri <sarabouri18@gmail.com>
2023-12-23 23:24:53 +0000

    SARA

---
+++ exe.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * execution - this function is used to execute command files from the root
+ * @cmd: command
+ * Return: 0.
+ */
+
+char execution(const char *cmd)
+{
+	pid_t process;
+	process = fork()
+	
+	if (process < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (process == 0)
+	{
+		execlp(cmd, cmd , (char *)NULL);
+		perror("execlp");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+	return (0);
+}
