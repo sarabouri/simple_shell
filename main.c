874012a326cbc42b77bd4101d01faa273b887f65
sarabouri <sarabouri18@gmail.com>
2023-12-24 00:44:52 +0000

    SARA

--- main.c
+++ main.c
@@ -38,6 +38,33 @@ void  user_input(char *command, size_t array)
 	}
 	command[strcspn(command, '\n')] = '\0';
 }
+/**
+ * execution - this function is used to execute command files from the root
+ * @cmd: command
+ * Return: 0.
+ */
+
+char execution(const char *cmd)
+{
+	pid_t process = fork();
+
+	if (process < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (process == 0)
+	{
+		execlp(cmd, cmd, (char *)NULL);
+		perror("execlp");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+	return (0);
+}
 
 /**
  * main - Entry point
