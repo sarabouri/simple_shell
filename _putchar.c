7e972b148538c16a6cb25324b142e665df4ea0d8
sarabouri <sarabouri18@gmail.com>
2024-01-07 22:23:51 +0000

    SARA

--- _putchar.c
+++ _putchar.c
@@ -11,3 +11,36 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * print_n - this function prints a number
+ * @m: this is an unsigned integer
+ * Return: void.
+ */
+void print_n(unsigned int m)
+{
+	unsigned int i = m;
+
+	if ((i / 10) > 0)
+		print_n(i / 10);
+
+	_putchar(i % 10 + '0');
+}
+/**
+ * ptint_n - this function prints nuber using putchar
+ * @m: this is an integer
+ * Return: void.
+ */
+void ptint_n(int m)
+{
+	unsigned int c = m;
+
+	if (m < 0)
+	{
+		_putchar('-');
+		c = -c;
+	}
+	if ((c / 10) > 0)
+			print_n(c / 10);
+
+	_putchar(c % 10 + '0');
+}
